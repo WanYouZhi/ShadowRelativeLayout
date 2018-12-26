e9bd411f64b055e5bef7dc030eafbeb276963818
906532015@qq.com <906532015@qq.com>
2018-12-26 14:31:26 +0800

    删除onlayout引起的的bug

--- app/src/main/java/com/wan/shadowrelativelayout/ShadowRelativeLayout.java
+++ app/src/main/java/com/wan/shadowrelativelayout/ShadowRelativeLayout.java
@@ -7,17 +7,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 /**
@@ -61,10 +56,6 @@ public class ShadowRelativeLayout extends RelativeLayout {
     super(context, attrs, defStyleAttr);
     dealAttrs(context, attrs);
     setPaint();
-  }
-
-  @Override protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
   }
 
   private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
