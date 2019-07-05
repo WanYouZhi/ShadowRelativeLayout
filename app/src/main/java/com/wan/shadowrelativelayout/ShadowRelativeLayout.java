7acf0cdfe1b73a038788df3a11332bbb5ea20613
万友志 <gwanyouzhi@derhino.com>
2019-07-05 15:23:11 +0800

    优化代码

--- app/src/main/java/com/wan/shadowrelativelayout/ShadowRelativeLayout.java
+++ app/src/main/java/com/wan/shadowrelativelayout/ShadowRelativeLayout.java
@@ -22,132 +22,147 @@ import android.widget.RelativeLayout;
  */
 
 public class ShadowRelativeLayout extends RelativeLayout {
-  /**
-   * 阴影的颜色, 需要带透明
-   */
-  private int shadowColor = Color.argb(90, 0, 0, 0);
-  /**
-   * 阴影的大小范围 shadowBlur越大越模糊，越小越清晰
-   */
-  private float shadowBlur = 30;
+    private static final int DEFAULT_BLUR = 30;
+    private static final int DEFAULT_ALPHA = 90;
+    /**
+     * 阴影的颜色, 需要带透明
+     */
+    private int shadowColor = Color.argb(DEFAULT_ALPHA, 0, 0, 0);
+    /**
+     * 阴影的大小范围 shadowBlur越大越模糊，越小越清晰
+     */
+    private float shadowBlur = DEFAULT_BLUR;
 
-  /**
-   * 阴影的圆角，只支持四角相同
-   */
-  private float shadowRadius = 0;
+    /**
+     * 阴影的圆角，只支持四角相同
+     */
+    private float shadowRadius = 0;
 
-  /**
-   * 阴影的偏移
-   */
-  private float shadowDx = 0;
-  private float shadowDy = 0;
+    /**
+     * 阴影的偏移
+     */
+    private float shadowDx = 0;
+    private float shadowDy = 0;
 
-  public ShadowRelativeLayout(@NonNull Context context) {
-    this(context, null);
-  }
-
-  public ShadowRelativeLayout(@NonNull Context context,
-      @Nullable AttributeSet attrs) {
-    this(context, attrs, 0);
-  }
-
-  public ShadowRelativeLayout(@NonNull Context context, @Nullable AttributeSet attrs,
-      int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-    dealAttrs(context, attrs);
-    setPaint();
-  }
-
-  private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-  @Override public void draw(Canvas canvas) {
-    setInsetBackground();
-    canvas.drawRoundRect(getRectF(), shadowRadius, shadowRadius, mPaint);
-    super.draw(canvas);
-  }
-
-  private boolean setInsetBackground() {
-    Drawable background = getBackground();
-    if (background == null || background instanceof InsetDrawable) {
-      return false;
+    public ShadowRelativeLayout(@NonNull Context context) {
+        this(context, null);
     }
-    InsetDrawable drawable =
-        new InsetDrawable(background, getPaddingLeft(), getPaddingTop(),
-            getPaddingRight(), getPaddingBottom());
-    setBackground(drawable);
-    return true;
-  }
 
-  private RectF getRectF() {
-    return new RectF(getPaddingLeft() + shadowDx, getPaddingTop() + shadowDy,
-        getWidth() - getPaddingRight() + shadowDx,
-        getHeight() - getPaddingBottom() + shadowDy);
-  }
-
-  private void dealAttrs(Context context, AttributeSet attrs) {
-    TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ShadowRelativeLayout);
-    if (typedArray != null) {
-      shadowColor = typedArray.getColor(R.styleable.ShadowRelativeLayout_shadow_color, shadowColor);
-      shadowRadius =
-          typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_radius, shadowRadius);
-      shadowBlur =
-          typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_blur, shadowBlur);
-      shadowDx = typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_dx, shadowDx);
-      shadowDy = typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_dy, shadowDy);
-      typedArray.recycle();
+    public ShadowRelativeLayout(@NonNull Context context,
+                                @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
     }
-  }
 
-  private void setPaint() {
-    setLayerType(View.LAYER_TYPE_SOFTWARE, null);  // 关闭硬件加速,阴影才会绘制
-    // todo 从AttributeSet获取设置的值
-    mPaint.setAntiAlias(true);
-    mPaint.setColor(shadowColor);
-    mPaint.setMaskFilter(new BlurMaskFilter(shadowBlur, BlurMaskFilter.Blur.NORMAL));
-  }
+    private boolean xmlBackground = true;
 
-  public int getShadowColor() {
-    return shadowColor;
-  }
+    public ShadowRelativeLayout(@NonNull Context context, @Nullable AttributeSet attrs,
+                                int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        xmlBackground = false;
+        setBackground(originBackground);
+        dealAttrs(context, attrs);
+        setPaint();
+    }
 
-  public void setShadowColor(int shadowColor) {
-    this.shadowColor = shadowColor;
-  }
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-  public float getShadowBlur() {
-    return shadowBlur;
-  }
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawRoundRect(getRectF(), shadowRadius, shadowRadius, mPaint);
+        super.draw(canvas);
+    }
 
-  public void setShadowBlur(float shadowBlur) {
-    this.shadowBlur = shadowBlur;
-  }
+    private Drawable originBackground;
 
-  public float getShadowRadius() {
-    return shadowRadius;
-  }
+    public Drawable getInsetBackground() {
+        return super.getBackground();
+    }
 
-  public void setShadowRadius(float shadowRadius) {
-    this.shadowRadius = shadowRadius;
-  }
+    @Override
+    public Drawable getBackground() {
+        return originBackground;
+    }
 
-  public float getShadowDx() {
-    return shadowDx;
-  }
+    @Override
+    public void setBackground(Drawable background) {
+        originBackground = background;
+        if (xmlBackground) { //padding还没好
+            return;
+        }
+        if (background != null && !(background instanceof InsetDrawable)) {
+            InsetDrawable drawable =
+                    new InsetDrawable(background, getPaddingLeft(), getPaddingTop(),
+                            getPaddingRight(), getPaddingBottom());
+            background = drawable;
+        }
+        super.setBackground(background);
+    }
 
-  public void setShadowDx(float shadowDx) {
-    this.shadowDx = shadowDx;
-  }
+    private RectF getRectF() {
+        return new RectF(getPaddingLeft() + shadowDx, getPaddingTop() + shadowDy,
+                getWidth() - getPaddingRight() + shadowDx,
+                getHeight() - getPaddingBottom() + shadowDy);
+    }
 
-  public float getShadowDy() {
-    return shadowDy;
-  }
+    private void dealAttrs(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ShadowRelativeLayout);
+        if (typedArray != null) {
+            shadowColor = typedArray.getColor(R.styleable.ShadowRelativeLayout_shadow_color, shadowColor);
+            shadowRadius =
+                    typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_radius, shadowRadius);
+            shadowBlur =
+                    typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_blur, shadowBlur);
+            shadowDx = typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_dx, shadowDx);
+            shadowDy = typedArray.getDimension(R.styleable.ShadowRelativeLayout_shadow_dy, shadowDy);
+            typedArray.recycle();
+        }
+    }
 
-  public void setShadowDy(float shadowDy) {
-    this.shadowDy = shadowDy;
-  }
+    private void setPaint() {
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);  // 关闭硬件加速,阴影才会绘制
+        // todo 从AttributeSet获取设置的值
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(shadowColor);
+        mPaint.setMaskFilter(new BlurMaskFilter(shadowBlur, BlurMaskFilter.Blur.NORMAL));
+    }
 
+    public int getShadowColor() {
+        return shadowColor;
+    }
 
-  @Override public boolean isOpaque() { //纯色或图片做背景时,draw之前会有黑底色，
-    return false;
-  }
+    public void setShadowColor(int shadowColor) {
+        this.shadowColor = shadowColor;
+    }
+
+    public float getShadowBlur() {
+        return shadowBlur;
+    }
+
+    public void setShadowBlur(float shadowBlur) {
+        this.shadowBlur = shadowBlur;
+    }
+
+    public float getShadowRadius() {
+        return shadowRadius;
+    }
+
+    public void setShadowRadius(float shadowRadius) {
+        this.shadowRadius = shadowRadius;
+    }
+
+    public float getShadowDx() {
+        return shadowDx;
+    }
+
+    public void setShadowDx(float shadowDx) {
+        this.shadowDx = shadowDx;
+    }
+
+    public float getShadowDy() {
+        return shadowDy;
+    }
+
+    public void setShadowDy(float shadowDy) {
+        this.shadowDy = shadowDy;
+    }
 }
